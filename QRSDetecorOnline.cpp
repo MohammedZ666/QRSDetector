1bbd659fa42d06e0cf4a11ad9cd9b8a03ce44ef6
Abu Obaida Zishan <abuobaidazishan@pop-os.localdomain>
2022-07-02 22:07:08 +0600

    added bandpass filter

--- QRSDetecorOnline.cpp
+++ QRSDetecorOnline.cpp
@@ -1,7 +1,10 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
+#include <math.h>
 #include </home/abuobaidazishan/DevProjects/qrs_detector/utils.h>
+#include<bits/stdc++.h>
+
 using namespace std;
 
 // QRSDetectorOffline class initialisation method.
@@ -14,37 +17,37 @@ using namespace std;
 // Configuration parameters.
 
 
-vector<int> get_qrs_indices(vector<float> ecg_raw_data){
+vector<int> get_qrs_indices(vector<double> ecg_raw_data){
 
     int signal_frequency = 250;  // Set ECG device frequency in samples per second here.
-    float filter_lowcut {0.0f};
-    float filter_highcut {15.0f};
-    float filter_order {1};
+    double filter_lowcut {0.0f};
+    double filter_highcut {15.0f};
+    double filter_order {1};
 
     int integration_window {15};  // Change proportionally when adjusting frequency (in samples).
 
-    float findpeaks_limit {0.35};
-    float findpeaks_spacing {50};  // Change proportionally when adjusting frequency (in samples).
+    double findpeaks_limit {0.35};
+    double findpeaks_spacing {50};  // Change proportionally when adjusting frequency (in samples).
 
     int refractory_period{120};  // Change proportionally when adjusting frequency (in samples).
-    float qrs_peak_filtering_factor {0.125f};
-    float noise_peak_filtering_factor {0.125f};
-    float qrs_noise_diff_weight  {0.25};
+    double qrs_peak_filtering_factor {0.125f};
+    double noise_peak_filtering_factor {0.125f};
+    double qrs_noise_diff_weight  {0.25};
 
     // Loaded ECG data.
-    //float ecg_data_raw []{};
+    //double ecg_data_raw []{};
 
     // Measured and calculated values.
-    vector <float> filtered_ecg_measurements;
-    vector <float> differentiated_ecg_measurements;
-    vector <float> squared_ecg_measurements;
-    vector <float> integrated_ecg_measurements;
+    vector <double> filtered_ecg_measurements;
+    vector <double> differentiated_ecg_measurements;
+    vector <double> squared_ecg_measurements;
+    vector <double> integrated_ecg_measurements;
     vector <int> detected_peaks_indices;
-    vector <float> detected_peaks_values;
+    vector <double> detected_peaks_values;
 
-    float qrs_peak_value {0.0f};
-    float noise_peak_value {0.0f};
-    float threshold_value = {0.0f};
+    double qrs_peak_value {0.0f};
+    double noise_peak_value {0.0f};
+    double threshold_value = {0.0f};
 
     // Detection results.
     // qrs_peaks_indices = np.array([], dtype=int)
@@ -56,9 +59,9 @@ vector<int> get_qrs_indices(vector<float> ecg_raw_data){
     return detected_peaks_indices;
 }
 
-vector<float> load_data_from_file(const char* path){
+vector<double> load_data_from_file(const char* path){
     std::ifstream file(path);
-    vector<float> ecg_data;
+    vector<double> ecg_data;
     if (file.is_open()) {
         string line;
         bool is_first_line = true;
@@ -66,7 +69,7 @@ vector<float> load_data_from_file(const char* path){
             if(is_first_line) is_first_line = false;
             else{
                 size_t c_index = line.find(",", 0);
-                float ecg_value = stof(line.substr(c_index+1, line.length()));
+                double ecg_value = stod(line.substr(c_index+1, line.length()));
                 ecg_data.push_back(ecg_value);
             }
         }
@@ -75,8 +78,66 @@ vector<float> load_data_from_file(const char* path){
     return ecg_data;
 }
 
+static const double pi = 3.141592f ;
+static const double pi2 = 2.0f * pi ;
+static const double s = 3900.111805811;          // Sample rate
+
+void bandpassFilter( double f_hz,           // Filter centre frequency
+                     double bw_hz,          // Filter bandwidth
+                     const double *x,       // Pointer to input sample block
+                     double *y,             // Pointer to output buffer
+                     int n                 // Number of samples in sample block
+                )
+{
+    static double x_2 = 0.0f;                    // delayed x, y samples
+    static double x_1 = 0.0f;
+    static double y_1 = 0.0f;
+    static double y_2 = 0.0f;
+
+    static const double f = f_hz / s ;
+    static const double bw = bw_hz / s ;
+
+    static const double R = 1 - (3 * bw) ;
+
+    static const double Rsq = R * R ;
+    static const double cosf2 = 2 * cos(pi2 * f) ;
+
+    static const double K = (1 - R * cosf2 + Rsq ) / (2 - cosf2) ;
+
+    // static const double a0 = 1.0 - K ;
+    // static const double a1 = 2 * (K - R) * cosf2 ;
+    // static const double a2 = Rsq - K ;
+    // static const double b1 = 2 * R * cosf2 ;
+    // static const double b2 = -Rsq ;
+
+    static const double a0 = 1.0f;
+    static const double a1 = -1.73356294f;
+    static const double a2 = 0.77567951f;
+    static const double b0 = 0.11216024f;
+    static const double b1 = 0;
+    static const double b2 = -0.11216024 ;
+    
+    for( int i = 0; i < n; ++i)
+    {
+        // IIR difference equation
+        y[i] = (b0 * x[i] + b1 * x_1 + b2 * x_2 
+                         - ( a1 * y_1 + a2 * y_2))/a0;
+
+        // shift delayed x, y samples
+        x_2 = x_1;                             
+        x_1 = x[i];
+        y_2 = y_1 ;
+        y_1 = y[i];
+        printf("%0.8f\n", y[i]);
+    }
+}
 int main(){
-    vector<float> ecg_data_raw = load_data_from_file("ecg_data/ecg_data_1.csv");
-    get_qrs_indices(ecg_data_raw);
+    vector<double> ecg_data_raw = load_data_from_file("ecg_data/ecg_data_1.csv");
+    double x [ecg_data_raw.size()];
+    double y [ecg_data_raw.size()] = {};
+    copy(ecg_data_raw.begin(), ecg_data_raw.end(), x);
+    //get_qrs_indices(ecg_data_raw);
+    bandpassFilter(8.660254038f, 5.0f, x, y,  2719);
+    //for(int i=0;i<ecg_data_raw.size(); i++) cout<<" x "<< x[i] << " y "<< y[i]<<endl;
     return 0;
 }
---
+++ filter_test.py
@@ -0,0 +1,31 @@
+from scipy.signal import butter, lfilter
+import numpy as np
+def bandpass_filter(data, lowcut, highcut, signal_freq, filter_order):
+        """
+        Method responsible for creating and applying Butterworth filter.
+        :param deque data: raw data
+        :param float lowcut: filter lowcut frequency value
+        :param float highcut: filter highcut frequency value
+        :param int signal_freq: signal frequency in samples per second (Hz)
+        :param int filter_order: filter order
+        :return array: filtered data
+        """
+        """Constructs signal filter and uses it to given data set."""
+        nyquist_freq = 0.5 * signal_freq
+        low = lowcut / nyquist_freq
+        high = highcut / nyquist_freq
+        b, a = butter(filter_order, [low, high], btype="band")
+        y = lfilter(b, a, data)
+        return y
+def load_ecg_data():
+    """
+    Method loading ECG data set from a file.
+    """
+    return np.loadtxt("ecg_data/ecg_data_1.csv", skiprows=1, delimiter=',')
+data = load_ecg_data()[:,1]
+data_filtered = bandpass_filter(data, 5, 15, 250, 1)
+data_filtered_cpp = np.loadtxt("filtered.txt")
+trunc = np.vectorize(lambda x : float('%.2f'%(x)))
+#print(data_filtered,'\n\n\nfiltered\n\n\n', data_filtered_cpp )
+data =trunc(data_filtered_cpp) - trunc(data_filtered) 
+print(sum(data))
